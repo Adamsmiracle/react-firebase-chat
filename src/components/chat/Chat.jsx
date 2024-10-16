c5d2dce0b2d478ce7faf087003bb4cd081ad5637
adams miracle <adamsmiracle0@gmail.com>
2024-10-16 21:49:26 +0000

    add the chat section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,11 +3,13 @@ import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  const [openEmoji, setOpenEmoji] = useState(true);
-  
-  const handleEmoji = (e) => {
-    console.log(e.emoji);
-  }
+    const [openEmoji, setOpenEmoji] = useState(false);
+    const [text, setText] = useState("");
+
+    const handleEmoji = (e) => {
+        setText((prev) => prev + e.emoji);
+        setOpenEmoji(false);
+    };
 
     return (
         <div className="chat">
@@ -25,20 +27,96 @@ const Chat = () => {
                     <img src="./info.png" />
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" />
+                    <div className="texts">
+                        <p>
+                            In lines of code, we build and dream, Where logic
+                            flows like data stream. From bits to bytes, the
+                            screen will glow, In every loop, new ideas grow.
+                        </p>
+                        <span>1 minute ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            In lines of code, we build and dream, Where logic
+                            flows like data stream. From bits to bytes, the
+                            screen will glow, In every loop, new ideas grow.
+                        </p>
+                        <span>1 minute ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" />
+                    <div className="texts">
+                        <p>
+                            In lines of code, we build and dream, Where logic
+                            flows like data stream. From bits to bytes, the
+                            screen will glow, In every loop, new ideas grow.
+                        </p>
+                        <span>1 minute ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            In lines of code, we build and dream, Where logic
+                            flows like data stream. From bits to bytes, the
+                            screen will glow, In every loop, new ideas grow.
+                        </p>
+                        <span>1 minute ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" />
+                    <div className="texts">
+                        <p>
+                            In lines of code, we build and dream, Where logic
+                            flows like data stream. From bits to bytes, the
+                            screen will glow, In every loop, new ideas grow.
+                        </p>
+                        <span>1 minute ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="./bg.jpg"/>
+                        
+                        <p>
+                            In lines of code, we build and dream, Where logic
+                            flows like data stream. From bits to bytes, the
+                            screen will glow, In every loop, new ideas grow.
+                        </p>
+                        <span>1 minute ago</span>
+                    </div>
+                </div>
+            </div>
             <div className="bottom">
                 <div className="icons">
                     <img src="./img.png" />
                     <img src="./camera.png" />
                     <img src="./mic.png" />
                 </div>
-                <input type="text" placeholder="Type a message..." />
+                <input
+                    type="text"
+                    placeholder="Type a message..."
+                    onChange={(e) => setText(e.target.value)}
+                    value={text}
+                />
                 <div className="emoji">
                     <img
                         src="./emoji.png"
                         onClick={() => setOpenEmoji((prev) => !prev)}
                     />
-                    <EmojiPicker open={openEmoji} onEmojiClick={handleEmoji} />
+                    <div className="picker">
+                        <EmojiPicker
+                            open={openEmoji}
+                            onEmojiClick={handleEmoji}
+                        />
+                    </div>
                 </div>
                 <button className="sendButton">Send</button>
             </div>
